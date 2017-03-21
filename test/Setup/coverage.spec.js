235fbd2bce6b17e42e56b71824db0823c539c2c6
Matheus Carmo <mematheuslc@gmail.com>
2017-03-21 17:53:16 -0300

    test: Setup test

--- test/Setup/coverage.spec.js
+++ test/Setup/coverage.spec.js
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 
 describe('Ok. Let\'s do it.', () => {
-   const phrase = 'May the force with you';
+   const phrase = 'May the code with you';
 
-   it('Go go go', () => assert.equal(phrase, 'May the force with you'));
+   it('Go go go', () => assert.equal(phrase, 'May the code with you'));
 });
