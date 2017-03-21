e928f4fcf571e5c0ced4f936b138e2f4fd4bc42a
Matheus Carmo <mematheuslc@gmail.com>
2017-03-21 20:33:29 -0300

    test(UserRepository): UserRepository test

---
+++ test/User/UserRepository.spec.js
@@ -0,0 +1,45 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import 'sinon-mongoose';
+import UserSchema from '../../src/Schemas/UserSchema';
+import UserRepository from '../../src/User/UserRepository';
+import mongoose from 'mongoose';
+
+mongoose.Promise = global.Promise;
+
+describe('Short URL Repository', () => {
+  let Repository;
+
+  before(() => {
+    Repository = new UserRepository();
+  });
+
+  it('Should save a new User', () => {
+    const date = new Date();
+
+    let User = new UserSchema({
+      name: 'Matheus',
+      birth_date: date
+    });
+
+    sinon.stub(User, 'save');
+
+    Repository.persist(User);
+
+    sinon.assert.calledWith(User.save);
+  });
+
+  it('Should get User', () => {
+    let User = new UserSchema({
+      name: 'Matheus'
+    });
+
+    sinon.stub(UserSchema, 'findOne').returns(
+      User
+    );
+
+    const UserFinded = Repository.getUser(User._id);
+
+    expect(UserFinded._id).to.equal(User._id);
+  });
+});
