a1f3540b59043e1a50034e1e36e45adbe676741f
Matheus Carmo <mematheuslc@gmail.com>
2017-03-21 20:21:00 -0300

    feat(UserSchema): Add birth_date to UserSchema

--- src/Schemas/UserSchema.js
+++ src/Schemas/UserSchema.js
@@ -1,18 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose  from 'mongoose';
 
-const User = new mongoose.Schema({
-    name: String,
-    birth_date: Date,
-    contacts: [{
-        type: {
-            type: String
-        },
-        value: {
-            type: String
-        }
-    }]
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  birth_date: Date,
+  contacts: [{
+    type: {
+      type: String
+    },
+    value: {
+      type: String
+    }
+  }]
 });
 
-User.index({name: 1});
+UserSchema.index({name: 1});
 
-export default mongoose.model('User', User);
+export default mongoose.model('User', UserSchema);
