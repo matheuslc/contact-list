ddc4cf9946ccce964c67472f8978e2349c231826
Matheus Carmo <mematheuslc@gmail.com>
2017-03-21 20:21:32 -0300

    feat(UserService): UserService implementation

---
+++ src/User/UserService.js
@@ -0,0 +1,27 @@
+import UserSchema from '../Schemas/UserSchema';
+
+export default class UserService {
+  /**
+   * @name createUser
+   * @param name
+   * @param birth_date
+   * @returns {Object} User Schema
+   */
+  createUser(name, birth_date) {
+    return new UserSchema({
+      name,
+      birth_date
+    });
+  }
+
+  /**
+   * @name deleteUser
+   * @param userId
+   * @returns {Promise}
+   */
+  deleteUser(userId) {
+    return UserSchema.remove({
+      _id: userId
+    });
+  }
+}
