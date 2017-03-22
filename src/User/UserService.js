88d7dfd25e5a858db8fb38f9be2bf50ae13d22b2
Matheus Carmo <mematheuslc@gmail.com>
2017-03-22 10:33:45 -0300

    feat(user): Add Contact CRUD

--- src/User/UserService.js
+++ src/User/UserService.js
@@ -4,20 +4,20 @@ export default class UserService {
   /**
    * @name createUser
    * @param name
-   * @param birth_date
+   * @param birthDate
    * @returns {Object} User Schema
    */
-  createUser(name, birth_date) {
+  createUser(name, birthDate) {
     return new UserSchema({
       name,
-      birth_date
+      birthDate
     });
   }
 
   /**
    * @name deleteUser
    * @param userId
-   * @returns {Promise}
+   * @returns {Query/*}
    */
   deleteUser(userId) {
     return UserSchema.remove({
@@ -25,9 +25,82 @@ export default class UserService {
     });
   }
 
-  updateUser(userId, newUser) {
-    return UserSchema.findOneAndUpate({
+  /**
+   * @name updateUser
+   * @param userId
+   * @param fields
+   * @returns {Query/*}
+   */
+  updateUser(userId, fields) {
+    return UserSchema.findByIdAndUpdate(userId, {
+      $set: {
+        name: fields.name,
+        birthDate: fields.birthDate
+      }
+    }, {
+      new: true
+    });
+  }
+
+  /**
+   * @name addContact
+   * @param userId
+   * @param type
+   * @param value
+   * @returns {Query|*}
+   */
+  addContact(userId, type, value) {
+    return UserSchema.findOneAndUpdate({
       _id: userId
-    }, newUser);
+    }, {
+      $push: {
+        contacts: {
+          type,
+          value
+        }
+      }
+    }, {
+      new: true
+    });
+  }
+
+  /**
+   * @name removeContact
+   * @param userId
+   * @param type
+   * @returns {Query|*}
+   */
+  removeContact(userId, type) {
+    return UserSchema.findOneAndUpdate({
+      _id: userId
+    }, {
+      $pull: {
+        contacts: {
+          $eq: type
+        }
+      }
+    }, {
+      new: true
+    });
+  }
+
+  /**
+   * @name getContact
+   * @param userId
+   * @param type
+   * @returns {Query|T|*}
+   */
+  getContact(userId, type) {
+    return UserSchema.find({
+      _id: userId
+    }, {
+      contacts: {
+        $elemMatch: {
+          type: {
+            $eq: type
+          }
+        }
+      }
+    });
   }
 }
